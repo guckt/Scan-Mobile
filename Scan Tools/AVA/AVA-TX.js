faf464d8af698a789927617411a712882af7275c
guckt <95729378+guckt@users.noreply.github.com>
2021-12-23 22:06:17 -0500

    Update AVA-TX.js

--- Scan Tools/AVA/AVA-TX.js
+++ Scan Tools/AVA/AVA-TX.js
@@ -11,19 +11,23 @@
 
 $(document).ready(function() {
 
-var addresses = [];
+    var tickers = [];
+    var tokens = [];
+    var addresses = [];
 
-var links =['https://snowtrace.io/token/',
-            'https://dexscreener.com/avalanche/',
-            'https://traderjoexyz.com/#/trade?outputCurrency='];
-
-var ignore =['0x130966628846bfd36ff31a822705796e8cb8c18d', //MIM
-             '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7', //WAVAX
-             '0xc7198437980c041c805a1edcba50c1ce5db95118', //USDTe
-             '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7', //USDT
-             '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664', //USDC
-             '0x19860ccb0a68fd4213ab9d8266f7bbf05a8dde98', //BUSD
-             '0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab']; //WETH
+    /*
+    var links =['https://snowtrace.io/token/',
+                'https://dexscreener.com/avalanche/',
+                'https://traderjoexyz
+                .com/#/trade?outputCurrency='];
+    */
+    var ignore =['0x130966628846bfd36ff31a822705796e8cb8c18d', //MIM
+                 '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7', //WAVAX
+                 '0xc7198437980c041c805a1edcba50c1ce5db95118', //USDTe
+                 '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7', //USDT
+                 '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664', //USDC
+                 '0x19860ccb0a68fd4213ab9d8266f7bbf05a8dde98', //BUSD
+                 '0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab']; //WETH
 
     for (let i = 1; i < 20; i++) {
         let totalSelector = document.querySelectorAll("#wrapperContent > li:nth-child("+i+") > div > a")
@@ -37,9 +41,45 @@ var ignore =['0x130966628846bfd36ff31a822705796e8cb8c18d', //MIM
 
                 if (ignore.indexOf(address) == -1)
                 {
-                    addresses[i] = address;
-                    $(totalSelector[k]).append('<input type="button" value="X" id="BT'+i+'" >')
+                    var ticker
+                    var token
+                    var element = totalSelector[k].outerHTML;
 
+                    //console.info("round " + i + ": " + totalSelector[k].childNodes.length);
+                    if ((totalSelector[k].childNodes.length) == 1) {
+                        token = element.substring(
+                            element.indexOf(">") + 1,
+                            element.lastIndexOf("(")
+                        );
+                        ticker = element.substring(
+                            element.indexOf("(") + 1,
+                            element.lastIndexOf(")")
+                        );
+                    }
+                    else if ((totalSelector[k].childNodes.length) == 2){
+                        token = totalSelector[k].childNodes[0].getAttribute("data-original-title");
+                        ticker = element.substring(
+                            element.indexOf("(") + 1,
+                            element.lastIndexOf(")")
+                        );
+                    }
+                    else if ((totalSelector[k].childNodes.length) == 3){
+                        token = element.substring(
+                            element.indexOf(">") + 1,
+                            element.lastIndexOf("(")
+                        );
+                        ticker = totalSelector[k].childNodes[1].getAttribute("data-original-title");
+                    }
+                    else if ((totalSelector[k].childNodes.length) == 4){
+                        token = totalSelector[k].childNodes[0].getAttribute("data-original-title");
+                        ticker = totalSelector[k].childNodes[2].getAttribute("data-original-title")
+                    }
+
+                    tickers[i] = ticker;
+                    tokens[i] = token;
+                    addresses[i] = address;
+
+                    $(totalSelector[k].parentNode).append('<input type="button" value="X" id="BT'+i+'" >')
                     //$("#BT").css("position", "fixed").css("top", 1).css("left", 100);
                     $("#BT"+i).css("margin-left", "5px");
                     $("#BT"+i).css("border-radius", "16px");
@@ -47,9 +87,12 @@ var ignore =['0x130966628846bfd36ff31a822705796e8cb8c18d', //MIM
                     $("#BT"+i).css("background", "white");
                     $("#BT"+i).css("border-color", "DodgerBlue");
                     $("#BT"+i).click(function(){
-                        for(let j=1; links.length > j; j++){
-                            window.open(links[j]+addresses[i]);
-                        }
+
+                        window.open('https://web.telegram.org/z/');
+                        window.open('https://twitter.com/search?q=%22' + addresses[i] + '%22&src=typed_query&f=live');
+                        window.open('https://twitter.com/search?q=%22' + tokens[i] + '%22&src=typed_query&f=live');
+                        window.open('https://twitter.com/search?q=%22' + '$' + tickers[i] + '%22&src=typed_query&f=live');
+                        //}
                         //window.location = current;
                     });
                 }
@@ -108,6 +151,7 @@ var ignore =['0x130966628846bfd36ff31a822705796e8cb8c18d', //MIM
             }
         }
     }
+
     //$('#ContentPlaceHolder1_maintable > iframe').on('load', function() {
     //        let iframe = $('#ContentPlaceHolder1_maintable > iframe');
     //});
